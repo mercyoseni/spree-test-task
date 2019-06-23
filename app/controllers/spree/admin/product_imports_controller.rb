645dcd9eb240ef597a95a13441336c9c4ce56054
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-06-23 01:35:14 +0100

    Refactor to display error(s) based on row

--- app/controllers/spree/admin/product_imports_controller.rb
+++ app/controllers/spree/admin/product_imports_controller.rb
@@ -1,20 +1,17 @@
 module Spree
   module Admin
-    class ProductImportsController < Spree::Admin::BaseController
+    class ProductImportsController < ResourceController
       def create
-        import_init = Spree::ProductImport.new
-        import_init.file = params[:csv_file]
+        @product_import = Spree::ProductImport.new
+        @product_import.file = params[:csv_file]
 
-        if import_init.valid? &&
-          import_init.import(params[:csv_file]) &&
-          import_init.errors.full_messages.empty?
-          flash[:success] = 'Products imported successfully!'
+        if @product_import.valid? &&
+          @product_import.import(params[:csv_file]) &&
+          @product_import.errors.full_messages.empty?
+          flash.now[:success] = 'Products imported successfully!'
         else
-          flash[:error] = "#{ import_init.errors.full_messages.uniq.join }.
-            Fix error and try again."
+          flash.now[:error] = 'Fix error and try again.'
         end
-
-        redirect_to admin_products_url
       end
     end
   end
--- app/models/spree/product_import.rb
+++ app/models/spree/product_import.rb
@@ -1,26 +1,41 @@
 module Spree
   class ProductImport
+    require 'csv'
     include ActiveModel::Model
 
     attr_accessor :file
 
+    def row_count
+      @row_number
+    end
+
     def import(csv_file)
+      @row_number = 0
+
       CSV.foreach(csv_file.path, headers: true, col_sep: ';') do |row|
-        # skip invalid row but display the error after the import
-        # TODO: implement displaying the row number with the related error
-        next unless params = row_to_params(row)
+        params = row_to_params(row)
+        @row_number += 1
 
-        existing_product = Spree::Product.find_by_slug(row['slug'])
+        begin
+          Spree::Product.transaction do
 
-        product =
-          if existing_product.present?
-            update_product(existing_product, params) # update the existing product
-          else
-            create_product(params) # create product using the params
+            existing_product = Spree::Product.find_by_slug(row['slug'])
+
+            product =
+              if existing_product.present?
+                update_product(existing_product, params) # update the existing product
+              else
+                create_product(params) # create product using the params
+              end
+
+            if product
+              set_taxon(product, row['category']) if row['category'].present?
+              set_stock_total(product, row['stock_total']) if row['stock_total'].present?
+            end
           end
-
-        set_taxon(product, row['category']) if row['category'].present?
-        set_stock_total(product, row['stock_total']) if row['stock_total'].present?
+        rescue => e
+          errors.add(:base, "Row #{ @row_number }: #{ e.message }")
+        end
       end
 
       true
@@ -51,40 +66,29 @@ module Spree
     end
 
     def row_to_params(row)
-      required_params = %w(name price slug)
-
-      if required_params.all? { |param| row[param].present? }
-        {
-          name: row['name'],
-          description: row['description'],
-          price: row['price'].sub(',', '.').to_f, # convert price to float
-          available_on: row['availability_date'],
-          slug: row['slug'],
-          shipping_category_id: set_shipping_category.id # set default shipping category
-        }
-      else
-        errors.add(
-          :base,
-          "Please include all attributes. Name, Price and Slug can't be blank"
-        )
-        nil
-      end
+      {
+        name: row['name'],
+        description: row['description'],
+        price: row['price'] && row['price'].sub(',', '.').to_f, # convert price to float
+        available_on: row['availability_date'],
+        slug: row['slug'],
+        shipping_category_id: set_shipping_category.id # set default shipping category
+      }
     end
 
     def update_product(product, params)
-      product.update(params)
+      product.update!(params)
       product
     end
 
     def create_product(params)
-      product = Spree::Product.create(params)
-      product
+      Spree::Product.create!(params)
     end
 
     def set_taxon(product, name)
       categories_taxonomy = Spree::Taxonomy.first
       taxon = Spree::Taxon.find_by_name(name) ||
-      Spree::Taxon.create(name: name, taxonomy: categories_taxonomy)
+      Spree::Taxon.create!(name: name, taxonomy: categories_taxonomy)
 
       # add taxon only when it's not already linked
       product.taxons << taxon unless product.taxons.include?(taxon)
@@ -93,17 +97,17 @@ module Spree
     def set_stock_total(product, stock_total)
       variant = Spree::Variant.find_by_product_id(product.id)
       stock_location = Spree::StockLocation.first
-      stock_item = Spree::StockItem.find_or_create_by(
+      stock_item = Spree::StockItem.find_or_create_by!(
         variant_id: variant.id,
         stock_location: stock_location
       )
       stock_item.count_on_hand = stock_total
 
-      stock_item.save
+      stock_item.save!
     end
 
     def set_shipping_category
-      Spree::ShippingCategory.find_or_create_by(name: 'Default')
+      Spree::ShippingCategory.find_or_create_by!(name: 'Default')
     end
   end
 end
