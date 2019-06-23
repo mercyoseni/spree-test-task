4492b4c1beb48b3401b4f4fd7172f713568e5f43
Halimat Mercy Oseni <mercy.oseni@andela.com>
2019-06-23 03:47:51 +0100

    Redirect to products page if file is invalid

--- app/controllers/spree/admin/product_imports_controller.rb
+++ app/controllers/spree/admin/product_imports_controller.rb
@@ -5,12 +5,16 @@ module Spree
         @product_import = Spree::ProductImport.new
         @product_import.file = params[:csv_file]
 
+        # don't redirect to the new page if file is invalid
+        unless @product_import.valid?
+          flash[:error] = "#{ @product_import.errors.full_messages.join }. Fix error and try again."
+          return redirect_to admin_products_path
+        end
+
         if @product_import.valid? &&
           @product_import.import(params[:csv_file]) &&
           @product_import.errors.full_messages.empty?
           flash.now[:success] = 'Products imported successfully!'
-        else
-          flash.now[:error] = 'Fix error and try again.'
         end
       end
     end
